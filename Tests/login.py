023d1bce454f0d2c48c9b76fd03a63a71f04d978
oleksandrlitvincuk <litvinchucksasha@gmail.com>
2020-01-16 13:39:18 +0200

    нов

--- Tests/login.py
+++ Tests/login.py
@@ -5,6 +5,8 @@ from Pages.LoginPage import loginPage
 from Pages.HomePage import HomePage
 from Pages.Passwards import Passwords
 import HtmlTestRunner
+import sys
+sys.path.append("/Users/oleksandrlitvincuk/Desktop/Python/PayOp")
 
 class LoginTest(unittest.TestCase):
 
@@ -22,6 +24,8 @@ class LoginTest(unittest.TestCase):
         login.enter_username(Passwords.corect_username)
         login.enter_password(Passwords.corect_password)
         login.click_login_buton()
+        #time.sleep(0.9)
+        assert self.driver.title == "PayOp - Merchant Panel"
 
         homepage = HomePage(driver)
         homepage.clik_on_email()
